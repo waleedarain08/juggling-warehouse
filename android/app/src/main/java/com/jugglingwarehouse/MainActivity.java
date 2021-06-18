8419d680719664072af01fdc7912cead1680324d
AsifAli2002 <85997100+AsifAli2002@users.noreply.github.com>
2021-06-18 21:20:20 +0500

    splash added

--- android/app/src/main/java/com/jugglingwarehouse/MainActivity.java
+++ android/app/src/main/java/com/jugglingwarehouse/MainActivity.java
@@ -1,15 +1,35 @@
 package com.jugglingwarehouse;
 
+import android.os.Bundle; // here 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen; // here \
+
 
 public class MainActivity extends ReactActivity {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  @Override
-  protected String getMainComponentName() {
-    return "JugglingWarehouse";
-  }
+ @Override
+   protected String getMainComponentName() {
+     return "JugglingWarehouse";
+   }
+
+ @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here 
+        super.onCreate(savedInstanceState);
+    }
 }
+// package com.jugglingwarehouse;
+
+// import com.facebook.react.ReactActivity;
+
+// public class MainActivity extends ReactActivity {
+
+//   /**
+//    * Returns the name of the main component registered from JavaScript. This is used to schedule
+//    * rendering of the component.
+//    */
+//   @Override
+//   protected String getMainComponentName() {
+//     return "JugglingWarehouse";
+//   }
+// }
