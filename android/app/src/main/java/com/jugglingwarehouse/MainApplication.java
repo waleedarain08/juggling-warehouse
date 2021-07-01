0e1b595fb454d90b0727b19bea5971fe54cdf614
AsifAli2002 <85997100+AsifAli2002@users.noreply.github.com>
2021-07-01 15:55:15 +0500

    splash android added

--- android/app/src/main/java/com/jugglingwarehouse/MainApplication.java
+++ android/app/src/main/java/com/jugglingwarehouse/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -23,6 +24,7 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
+          new SplashScreenReactPackage();
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
